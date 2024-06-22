d4e7d4f161965e6e4375ac3b6ebcd5b4b04d4d19
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-06-22 15:51:23 +0330

    create app core & installed it

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party packages
+    'rest_framework',
+
+    # local apps
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
