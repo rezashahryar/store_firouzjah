949e5870a6ed1e08b83742cedd30cf154669dbe8
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-15 11:21:59 +0330

    create model category product & sub category product

--- store/models.py
+++ store/models.py
@@ -8,3 +8,19 @@ from django.db import models
 
 class Store(models.Model):
     ...
+
+
+class CategoryProduct(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+    
+
+class SubCategoryProduct(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
