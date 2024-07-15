7df146a04d78f9771184d79469c8a3f164702455
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-15 11:23:10 +0330

    create model product type & product

--- store/models.py
+++ store/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 # create your models here
 
 
@@ -24,3 +21,34 @@ class SubCategoryProduct(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class ProductType(models.Model):
+    category = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, related_name='products_type')
+    sub_category = models.ForeignKey(SubCategoryProduct, on_delete=models.CASCADE, related_name='products_type')
+
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='products')
+
+    category = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, related_name='products')
+    sub_category = models.ForeignKey(SubCategoryProduct, on_delete=models.CASCADE, related_name='products')
+
+    title_farsi = models.CharField(max_length=255)
+    title_english = models.CharField(max_length=255)
+    
+    product_code = models.CharField(max_length=100)
+    product_model = models.CharField(max_length=255)
+
+    status_originaly = models.CharField(max_length=10)
+    product_warranty = models.BooleanField()
+
+    sending_method = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title_farsi
