6b09bf88340cfc84bbd74a71758cf340a19b2b7c
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-15 11:55:43 +0330

    display models in admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from store import models
+
 # Register your models here.
+
+
+@admin.register(models.Store)
+class StoreAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.CategoryProduct)
+class CategoryProductAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.SubCategoryProduct)
+class SubCategoryProductAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    ...
