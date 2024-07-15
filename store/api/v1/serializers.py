f55dfa13feb2cc659af1283d39cb81f1ea1746a7
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-15 19:34:48 +0330

    reformatting serializers.py in store app

--- store/api/v1/serializers.py
+++ store/api/v1/serializers.py
@@ -10,12 +10,40 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Product
-        fields = ['title_farsi', 'title_english', 'category', 'product_code', 'product_model', 'status_originaly', 'product_warranty', 'sending_method']
+        fields = [
+            "title_farsi",
+            "title_english",
+            "category",
+            "product_code",
+            "product_model",
+            "status_originaly",
+            "product_warranty",
+            "sending_method",
+        ]
 
 
 class StoreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Store
-        fields = ['mobile_number', 'phone_number', 'email', 'shomare_shaba', 'province', 'city', 'mantaghe', 'mahalle', 'address', 'post_code', 'parvane_kasb', 'tasvire_personely', 'kart_melli', 'shenasname', 'logo', 'roozname_rasmi_alamat', 'gharardad', 'code']
-        read_only_fields = ['code']
+        fields = [
+            "mobile_number",
+            "phone_number",
+            "email",
+            "shomare_shaba",
+            "province",
+            "city",
+            "mantaghe",
+            "mahalle",
+            "address",
+            "post_code",
+            "parvane_kasb",
+            "tasvire_personely",
+            "kart_melli",
+            "shenasname",
+            "logo",
+            "roozname_rasmi_alamat",
+            "gharardad",
+            "code",
+        ]
+        read_only_fields = ["code"]
