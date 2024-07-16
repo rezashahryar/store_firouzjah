b8172b25baa60f05283a3ba575d28b4a6c472552
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-17 02:07:09 +0330

    add static func to display images

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
@@ -29,6 +31,8 @@ urlpatterns = [
     path('auth/', include('core.urls')),
     path('store/', include('store.urls')),
 ] + debug_toolbar_urls()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 # config for swagger
