1a6eb76fed1f7cbd5f832d62c4b984ab9fe73385
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-21 01:55:47 +0330

    change type of save date time now in otp request model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,17 @@
 from datetime import timedelta
 from uuid import uuid4
 
+import django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+import django.utils
+import django.utils.timezone
 
 # Create your models here.
+
+def get_datetime_now():
+    return timezone.now() + timedelta(minutes=3)
 
 
 class User(AbstractUser):
@@ -17,5 +23,5 @@ class OtpRequest(models.Model):
     mobile = models.CharField(max_length=11)
     otp_code = models.CharField(max_length=5)
 
-    valid_from = models.DateTimeField(default=timezone.now(), null=True)
-    valid_until = models.DateTimeField(default=timezone.now() + timedelta(minutes=3), null=True)
+    valid_from = models.DateTimeField(default=django.utils.timezone.now, null=True)
+    valid_until = models.DateTimeField(default=get_datetime_now, null=True)
