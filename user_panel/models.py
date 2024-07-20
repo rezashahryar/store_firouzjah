46c1c2408f6518d0979c19fc648bb3e7c5ee54f2
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-21 01:56:36 +0330

    create profile model & versioning app user_panel

--- user_panel/models.py
+++ user_panel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
